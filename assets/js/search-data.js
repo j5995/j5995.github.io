f6f018e8ee4982886410334a519c2ed7a8c2d3cd
j5995 <145726848+j5995@users.noreply.github.com>
2025-10-27 00:01:46 +0000

    Deploying to gh-pages from @ j5995/j5995.github.io@eeb225e6712edb9edb4df2eb63e417a764ab4f0c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,9 +466,9 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-human-benchmark-knockoff",
+          title: 'Human Benchmark Knockoff',
+          description: "Group Project created for CSE110",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
